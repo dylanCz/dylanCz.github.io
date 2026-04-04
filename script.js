07ce167b63f6bcdd75df4d447158c1ae3f620f85
Dylan Clark <github@dylanclark.dev>
2026-04-04 18:01:57 +0100

    Add some basic js for a consistent header

---
+++ script.js
@@ -0,0 +1,24 @@
+customElements.define(
+  "site-header",
+  class extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <header class="header">
+            <div class="header-inner">
+                <a href="/" class="header-left">Dylan Clark</a>
+
+                <input type="checkbox" id="nav-toggle" class="nav-toggle" />
+                <label for="nav-toggle" class="nav-burger">&#9776;</label>
+
+                <nav class="header-right">
+                    <a href="about.html">About</a>
+                    <a href="projects.html">Projects</a>
+                    <a href="log.html">Log</a>
+                    <a href="resume.html">Resume</a>
+                </nav>
+            </div>
+        </header>
+    `;
+    }
+  },
+);
